4bfe7951fdc025be197502ea8c773d6024d6020a
Kapil <Kapilbiloniya4444@gmail.com>
2025-03-27 23:24:17 +0530

    Updating JavaPractice.java

--- Practice/JavaPractice.java
+++ Practice/JavaPractice.java
@@ -1,31 +1,8 @@
-//Practicing java in this file
-
 import java.util.*;
 
-public class JavaPractice {
-    public static void main(String args[]) {  
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+ public class JavaPractice {
+    public static void main(String args[]) {
 
-        if(n == 2){
-            System.out.print("Prime Number");
-        } 
-        else{
-            boolean isPrime = true;
-            for(int i=2; i<=n-1; i++){
-                if(n%i == 0){
-                    isPrime = false;
-                }
-            }
-
-            if(isPrime == true){
-                System.out.print("Is Prime Number");
-            }
-            else{
-                System.out.print("Is Not Prime Number");
-            }
-        }
-        
-
+    
     }
 }
