17a38e5865741102bf2eee5b3a3601eddfef9a32
Kapil <Kapilbiloniya4444@gmail.com>
2025-03-27 23:54:17 +0530

    Practicing in Java file

--- Practice/JavaPractice.java
+++ Practice/JavaPractice.java
@@ -1,8 +1,59 @@
 import java.util.*;
 
- public class JavaPractice {
-    public static void main(String args[]) {
+// // While Loop
 
+//  public class JavaPractice {
+//     public static void main(String args[]) {
+
+//         int i = 1;
+//         while(i <= 10){
+//             System.out.println(i);
+//             i++;
+//         }
     
-    }
-}
+//     }
+// }
+
+// // For loop
+
+// public class JavaPractice {
+//     public static void main(String args[]) {
+
+//         for(int i=1; i<=10; i++){
+//             System.out.println(i);
+//         }
+
+//     }
+// }
+
+// // Do While Loop
+
+// public class JavaPractice {
+//     public static void main(String args[]){
+
+//         int i=1;
+//         do{
+//             System.out.println(i);
+//             i++;
+//         }while(i <= 10);
+
+//     }
+// }
+
+// Break & Continue Statement
+
+// public class JavaPractice {
+//     public static void main(String args[]) {  
+
+//        int i = 1;  // Initialization
+//        while(i <= 10) {  // Condition
+//            if(i == 5) {
+//                i++; // Increment before continue to avoid an infinite loop
+//                continue;
+//            }
+//            System.out.println(i);
+//            i++; // Increment
+//        }
+//     }
+// }
+
