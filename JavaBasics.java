75753ade2e016b48f27210e2ec704b94c2f397cc
Kapil <Kapilbiloniya4444@gmail.com>
2025-04-06 20:51:04 +0530

    Updating

--- JavaBasics.java
+++ JavaBasics.java
@@ -2112,3 +2112,48 @@ public class JavaBasics{
         
     }
 }
+
+// TRAPPED RAIN WATER (DSA MEDIAN LEVEL QUESTION)
+
+import java.util.*;
+
+public class JavaBasics{
+    public static int trappedRainWater(int height[]){
+
+        //Calculation Left Max Boundary
+        int leftMax[] = new int[height.length];
+        leftMax[0] = height[0];
+
+        for(int i = 1; i < height.length; i++){
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+
+        //Calculation Right Max Boundary
+        int rightMax[] = new int[height.length];
+        rightMax[height.length - 1] = height[height.length - 1];
+
+        for(int i = height.length - 2; i >= 0; i--){
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+
+        int trappedWater = 0;
+
+        //loop
+        for(int i = 0; i < height.length; i++){
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+
+            trappedWater += waterLevel - height[i];
+        }
+
+        return trappedWater;
+
+    }
+
+    public static void main(String args[]){
+        int height[] = {4, 2, 0, 6, 3, 2, 5};
+        System.out.print("Trappped Water = " + trappedRainWater(height));
+    }
+}
+
+// BEST TIME TO BUY AND SELL STOCK
+
--- Practice/JavaPractice.java
+++ Practice/JavaPractice.java
@@ -1167,28 +1167,69 @@
 //     }
 // }
 
-public class JavaPractice{
-    public static void sort(int array[]){
-        for(int i = 0; i < array.length; i++){
-            for(int j = i + 1; j < array.length; j++){
-                if(array[i] < array[j]){
-                    int temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
-                }
-            }
-        }
+// public class JavaPractice{
+//     public static void sort(int array[]){
+//         for(int i = 0; i < array.length; i++){
+//             for(int j = i + 1; j < array.length; j++){
+//                 if(array[i] < array[j]){
+//                     int temp = array[j];
+//                     array[j] = array[i];
+//                     array[i] = temp;
+//                 }
+//             }
+//         }
 
-    }
+//     }
 
-    public static void main(String args[]){
-        int array[] = {5, 2, 4, 1, 6, 3};
-        sort(array);
+//     public static void main(String args[]){
+//         int array[] = {5, 2, 4, 1, 6, 3};
+//         sort(array);
         
-        System.out.print("Sorted Array = ");
+//         System.out.print("Sorted Array = ");
 
-        for(int i = 0; i < array.length; i++){
-            System.out.print(array[i] + " ");
-        }
-    }
-}
+//         for(int i = 0; i < array.length; i++){
+//             System.out.print(array[i] + " ");
+//         }
+//     }
+// }
+
+// import java.util.*;
+
+// public class JavaPractice{
+//     public static int trappedRainWater(int height[]){
+
+//         //Calculation Left Max Boundary
+//         int leftMax[] = new int[height.length];
+//         leftMax[0] = height[0];
+
+//         for(int i = 1; i < height.length; i++){
+//             leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+//         }
+
+//         //Calculation Right Max Boundary
+//         int rightMax[] = new int[height.length];
+//         rightMax[height.length - 1] = height[height.length - 1];
+
+//         for(int i = height.length - 2; i >= 0; i--){
+//             rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+//         }
+
+//         int trappedWater = 0;
+
+//         //loop
+//         for(int i = 0; i < height.length; i++){
+//             int waterLevel = Math.min(leftMax[i], rightMax[i]);
+
+//             trappedWater += waterLevel - height[i];
+//         }
+
+//         return trappedWater;
+
+//     }
+
+//     public static void main(String args[]){
+//         int height[] = {4, 2, 0, 6, 3, 2, 5};
+//         System.out.print("Trappped Water = " + trappedRainWater(height));
+//     }
+// }
+
