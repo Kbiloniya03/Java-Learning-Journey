6b47bdc43466be1d68ed0012dfe238da836c4d9f
Kapil <Kapilbiloniya4444@gmail.com>
2025-04-06 22:04:56 +0530

    Updating

--- JavaBasics.java
+++ JavaBasics.java
@@ -2157,3 +2157,61 @@ public class JavaBasics{
 
 // BEST TIME TO BUY AND SELL STOCK
 
+// PRACTICE QUESTIONS
+
+// 1. Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// CASE I [1,2,3,1] = TRUE
+// CASE II [1,2,3,4] = FALSE
+// CASE III [1, 1, 1, 3, 3, 4, 3, 2, 4, 2] = TRUE
+
+public class JavaBasics{
+    public static boolean isNumRepeat(int nums[]){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]){
+        int nums[] ={1, 2, 3, 4};
+        System.out.println(isNumRepeat(nums));
+    }
+}
+
+// 2. There is an integer array nums sorted in ascending order (with distinct values).Prior to being passed to your function, nums is possibly rotated at an unknown pivot  index k (1  <=  k  <  nums.length)  such  that  the  resulting  array  is [nums[k], nums[k+1],   ...,   nums[n-1], nums[0],   nums[1],   ...,   nums[k-1]] (0-indexed).   For   example, [0,1,2,4,5,6,7] might        be        rotated        at        pivot        index 3   and become [4,5,6,7,0,1,2].Given the array nums after the possible rotation and an integer target, returnthe index oftarget if it is in nums, or -1   if it is not in nums.You must write an algorithm with O(log n) runtime complexity.
+
+// CASE I   Input: nums = [4,  5, 6, 7, 0, 1, 2], target = 0 Output:   4
+// CASE II  Input: nums = [4,  5, 6, 7, 0, 1, 2], target = 3 Output:   -1
+// CASE III Input: nums = [1], target = 0 Output:   -1 
+
+public class JavaBasics{
+    public static int target(int nums[], int key){
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]){
+        int nums[] = {4, 5, 6, 7, 0, 1, 2};
+        int key = 0;
+        int index = target(nums, key);
+        System.out.print(index);
+
+    }
+}
+
+// 5. Given an integer array nums, return all the triplets [nums[i], nums[j],  nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.Notice that the solution set must not contain duplicate triplets.
+
+// CASE I   Input: nums = [-1, 0,  1, 2, -1, -4]   Output:   [ [-1, -1, 2] , [-1, 0, 1] ]
+// CASE II  Input: nums = [ ]  Output:   [ ] 
+// CASE III Input: nums = [ 0 ]  Output:   [ ]
+
+
--- Practice/JavaPractice.java
+++ Practice/JavaPractice.java
@@ -1233,3 +1233,40 @@
 //     }
 // }
 
+// public class JavaPractice{
+//     public static boolean isNumRepeat(int nums[]){
+//         for(int i = 0; i < nums.length; i++){
+//             for(int j = i + 1; j < nums.length; j++){
+//                 if(nums[i] == nums[j]){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+
+//     public static void main(String args[]){
+//         int nums[] ={1, 2, 3, 4};
+//         System.out.println(isNumRepeat(nums));
+//     }
+// }
+
+// public class JavaPractice{
+//     public static int target(int nums[], int key){
+//         for(int i = 0; i < nums.length; i++){
+//             if(nums[i] == key){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+
+//     public static void main(String args[]){
+//         int nums[] = {4, 5, 6, 7, 0, 1, 2};
+//         int key = 0;
+//         int index = target(nums, key);
+//         System.out.print(index);
+
+//     }
+// }
+
