fbf0dae1da2a5752bd17af6d3848f2726aabb4ed
Kapil <Kapilbiloniya4444@gmail.com>
2025-04-13 17:08:18 +0530

    Updating

--- JavaBasics.java
+++ JavaBasics.java
@@ -3149,6 +3149,33 @@ public class JavaBasics {
 
 // STRING COMPRESSION
 
+public class JavaBasics{
+    public static String compress(String str){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < str.length(); i++){
+            Integer count = 1;
+            while(i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)){
+                count++;
+                i++;
+            }
+            if(count > 1){
+                sb.append(str.charAt(i));
+                sb.append(count.toString());
+            }
+            else{
+                sb.append(str.charAt(i));
+            }
+        }
+
+        return sb.length() > str.length() ? str : sb.toString();
+    }
+    public static void main(String args[]){
+       String str = "aaabbcccdd";
+       System.out.println(compress(str));
+    }
+}
+
 // PRACTICE QUESTIONS
 // Question1: Count how many times lowercase vowels occurred in a String entered by the user
 
@@ -3320,3 +3347,4 @@ public class JavaBasics{
         isAnagram(str1, str2);
     }
 }
+
--- Practice/JavaPractice.java
+++ Practice/JavaPractice.java
@@ -2081,3 +2081,29 @@
 //     }
 // }
 
+// public class JavaPractice{
+//     public static String compress(String str){
+//         StringBuilder sb = new StringBuilder();
+
+//         for(int i = 0; i < str.length(); i++){
+//             Integer count = 1;
+//             while(i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)){
+//                 count++;
+//                 i++;
+//             }
+//             if(count > 1){
+//                 sb.append(str.charAt(i));
+//                 sb.append(count.toString());
+//             }
+//             else{
+//                 sb.append(str.charAt(i));
+//             }
+//         }
+
+//         return sb.length() > str.length() ? str : sb.toString();
+//     }
+//     public static void main(String args[]){
+//        String str = "aaabbcccdd";
+//        System.out.println(compress(str));
+//     }
+// }
